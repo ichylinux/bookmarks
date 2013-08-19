4ee8148b401292fae3b92ba831ca232d60f9f7ea
y-matsuda <yuuichi.matsuda@ulsystems.co.jp>
2013-08-19 12:26:53 +0900

    初回コミット

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookmarks::Application.config.secret_token = 'be7455d3800582b90b453aab050273a9ee49f354e9ed333002fb16a9c0706a8b42828f2d12ff34314adf1931d4cfb071fa3c93b20b9021f990bd7b87410015d1'
