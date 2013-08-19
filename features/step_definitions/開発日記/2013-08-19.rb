33e06bce288295132a018acd7b1f483ffc811145
ichy <ichylinux@gmail.com>
2013-08-19 22:14:31 +0900

    日記

---
+++ features/step_definitions/開発日記/2013-08-19.rb
@@ -0,0 +1,27 @@
+# coding: UTF-8
+
+前提 /^rails new bookmarks -d mysql --skip-bundle$/ do
+  `rm -Rf /tmp/bookmarks`
+  `cd /tmp && rails new bookmarks -d mysql --skip-bundle`
+  puts "<pre>#{`cd /tmp/bookmarks && tree .`}</pre>"
+end
+
+前提 /^Gemfileを編集$/ do
+  diff 'Gemfile', '/tmp/bookmarks/Gemfile', :as => 'edit'
+end
+
+前提 /^sudo bundle install$/ do
+  `cd /tmp/bookmarks && sudo bundle install`
+  diff 'Gemfile.lock', '/tmp/bookmarks/Gemfile.lock', :as => 'auto'
+end
+
+前提 /^rake dad:db:config$/ do
+  diff 'config/database.yml', '/tmp/bookmarks/config/database.yml', :as => 'auto'
+end
+
+前提 /^rake dad:db:create$/ do
+end
+
+前提 /^rake db:migrate$/ do
+  show 'db/schema.rb', :as => 'auto'
+end
