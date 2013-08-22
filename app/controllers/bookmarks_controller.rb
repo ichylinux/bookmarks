f024bb4caecb1b0cfcf46748daf52cb1218a08b7
y-matsuda <yuuichi.matsuda@ulsystems.co.jp>
2013-08-22 13:06:47 +0900

    ブックマークのCRUD

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,6 +3,49 @@
 class BookmarksController < ApplicationController
   
   def index
+    @bookmarks = Bookmark.not_deleted
   end
 
+  def show
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(params[:bookmark])
+    
+    @bookmark.transaction do
+      @bookmark.save!
+    end
+    
+    redirect_to :action => 'index'
+  end
+
+  def edit
+    @bookmark = Bookmark.find(params[:id])
+  end
+  
+  def update
+    @bookmark = Bookmark.find(params[:id])
+    
+    @bookmark.transaction do
+      @bookmark.attributes = params[:bookmark]
+      @bookmark.save!
+    end
+    
+    redirect_to :action => 'show', :id => @bookmark
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    
+    @bookmark.transaction do
+      @bookmark.destroy_logically!
+    end
+    
+    redirect_to :action => 'index'
+  end
 end
