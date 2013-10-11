bd9abc6c8b76d681aed72e5bbc2889ed1e77625a
y-matsuda <yuuichi.matsuda@ulsystems.co.jp>
2013-10-11 12:57:32 +0900

    RSS

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -4,12 +4,14 @@ class Feed < ActiveRecord::Base
 
   def feed
     begin
-      @feed ||= Feedzirra::Feed.fetch_and_parse(self.url)
+      @feed ||= Feedzirra::Feed.fetch_and_parse(self.url, :ssl_verify_host => false)
     rescue => e
+      Rails.logger.error e.message
+      @feed = false
     end
   end
 
   def entries
-    feed ? feed.entries : []
+    feed.is_a?(Feedzirra::Parser::RSS) ? feed.entries : []
   end
 end
