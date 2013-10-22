8f30a36514a81c8c30c4533dbf76a0c1b6701641
y-matsuda <yuuichi.matsuda@ulsystems.co.jp>
2013-10-22 12:35:17 +0900

    日記

--- features/step_definitions/開発日記/2013-10-22.rb
+++ features/step_definitions/開発日記/2013-10-22.rb
@@ -1,2 +1,11 @@
 # coding: UTF-8
 
+もし /^タイトルを取得するアクションを追加$/ do
+  git_diff 'config/routes.rb', :from => 59, :to => 63
+  git_diff 'app/controllers/feeds_controller.rb'
+end
+
+もし /^Ajaxで呼び出す$/ do
+  show 'app/assets/javascripts/feeds.js', :as => 'new'
+  git_diff 'app/views/feeds/_form.html.erb'
+end
