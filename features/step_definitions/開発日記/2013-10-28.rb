259d70147bfba643c2364655cfb59c56a7241cfb
y-matsuda <yuuichi.matsuda@ulsystems.co.jp>
2013-10-29 12:45:49 +0900

    日記

--- features/step_definitions/開発日記/2013-10-28.rb
+++ features/step_definitions/開発日記/2013-10-28.rb
@@ -3,3 +3,16 @@
 もし /^メールアドレスをクリックしてメニューが開くように修正$/ do
   git_diff 'app/views/common/_menu.html.erb'
 end
+
+もし /^認証情報を保存するカラムを追加$/ do
+  git_diff 'db/schema.rb', :as => 'auto'
+  git_diff 'config/locales/ja.yml'
+end
+
+もし /^フォームに入力項目を追加$/ do
+  git_diff 'app/views/feeds/_form.html.erb'
+end
+
+もし /^フィードを取得時にBasic認証を設定$/ do
+  git_diff 'app/models/feed.rb'
+end
