5f984ac1d6233325727715709cb1fa23face2779
y-matsuda <yuuichi.matsuda@ulsystems.co.jp>
2013-10-29 12:35:03 +0900

    リファクタリング

--- app/assets/javascripts/feeds.js
+++ app/assets/javascripts/feeds.js
@@ -1,14 +1,22 @@
 var feeds = {};
 
 feeds.get_feed_title = function(button) {
-  var form = $(button).closest('form'); 
+  var form = $(button).closest('form');
+
   var url = form.find('input[name*="url"]').val();
   if (url == '') {
     alert('フィードURLを先に入力してください。');
     return;
   }
   
-  $.post($(button).data('url'), {url: url}, function(title) {
+  var params = form.serializeArray();
+  for (var i = 0; i < params.length; i ++) {
+    if (params[i].name == 'id' || params[i].name == '_method') {
+      params[i].value = null;
+    }
+  }
+
+  $.post($(button).data('url'), params, function(title) {
     if ($.trim(title) == '') {
       alert('フィードを取得できませんでした。');
       return;
