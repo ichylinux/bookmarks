f379da3f76b00429fdc3df20ffa926dcaca1860f
y-matsuda <yuuichi.matsuda@ulsystems.co.jp>
2013-11-07 12:15:08 +0900

    merge

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,9 +3,9 @@
 class WelcomeController < ApplicationController
 
   def index
-    @bookmarks = Bookmark.where(:user_id => current_user).not_deleted.order(:title)
+    @bookmarks = [] #Bookmark.where(:user_id => current_user).not_deleted.order(:title)
     @todos = Todo.where(:user_id => current_user).not_deleted.order(:priority, :title)
-    @feeds = Feed.where(:user_id => current_user).not_deleted
+    @feeds = [] #Feed.where(:user_id => current_user).not_deleted
   end
 
 end
