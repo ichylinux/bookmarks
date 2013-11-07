89b44c10ad1d9d813a08ce733c2bf5e15f3de287
y-matsuda <yuuichi.matsuda@ulsystems.co.jp>
2013-11-07 13:10:09 +0900

    p10

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,25 +3,9 @@
 class WelcomeController < ApplicationController
 
   def index
-    @gadgets = [[], [], []]
-    count = 0
-
     @bookmarks = Bookmark.where(:user_id => current_user).not_deleted.order(:title)
-    if @bookmarks.present?
-      @gadgets[count % 3] << BookmarkGadget.new(@bookmarks)
-      count += 1
-    end
-
     @todos = Todo.where(:user_id => current_user).not_deleted.order(:priority, :title)
-    if @todos.present?
-      @gadgets[count % 3] << TodoGadget.new(@todos)
-      count += 1
-    end
-    
-    Feed.where(:user_id => current_user).not_deleted.each do |f|
-      @gadgets[count % 3] << f
-      count += 1
-    end
+    @feeds = Feed.where(:user_id => current_user).not_deleted
   end
 
 end
