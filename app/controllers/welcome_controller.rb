8d31265818835eebb50e8eda749037f9607f8e64
y-matsuda <yuuichi.matsuda@ulsystems.co.jp>
2013-11-08 19:10:47 +0900

    master

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,6 +7,10 @@ class WelcomeController < ApplicationController
   end
 
   def save_state
+    ActiveRecord::Base.transaction do
+      Portal.new(current_user).update_layout(params[:portal])
+    end
+
     render :nothing => true
   end
 
--- app/models/portal.rb
+++ app/models/portal.rb
@@ -44,4 +44,21 @@ class Portal
     gadgets
   end
 
+  def update_layout(params = {})
+    now = Time.now
+
+    params.each do |column, gadget_ids|
+      column_no = column.split('_').last.to_i
+      
+      gadget_ids.each_with_index do |gadget_id, i|
+        pl = PortalLayout.where(:user_id => user.id, :column_no => column_no, :display_order => i).first
+        pl ||= PortalLayout.new(:user_id => user.id, :column_no => column_no, :display_order => i)
+        pl.gadget_id = gadget_id
+        pl.save!
+      end
+    end
+
+    PortalLayout.delete_all(['user_id = ? and updated_at < ?', user.id, now])
+  end
+
 end
