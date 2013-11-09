b405675630098c968b359232bf0d7aa87682c07e
ichy <ichylinux@gmail.com>
2013-11-09 12:42:59 +0900

    新しいスプリント

--- features/step_definitions/開発日記/2013-11-08.rb
+++
@@ -1,7 +0,0 @@
-# coding: UTF-8
-
-もし /^レイアウトが変更されたタイミングでサーバーにPOST$/ do
-  git_diff 'app/views/welcome/index.html.erb'
-  git_diff 'app/controllers/welcome_controller.rb'
-  show 'app/models/portal.rb', :as => 'new'
-end
