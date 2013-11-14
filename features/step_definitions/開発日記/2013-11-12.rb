0515df377caf2e4c9ef2a8592aef6a0f0772b6d6
y-matsuda <yuuichi.matsuda@ulsystems.co.jp>
2013-11-14 12:59:47 +0900

    日記

--- features/step_definitions/開発日記/2013-11-12.rb
+++ features/step_definitions/開発日記/2013-11-12.rb
@@ -4,6 +4,7 @@
   diff 'db/migrate/20131112032742_create_calendars.rb',
       File.join(File.dirname(__FILE__), '20131112032742_create_calendars.rb.txt'), :as => ['auto', 'edit']
   git_diff 'config/locales/ja.yml', :to => 10
+  git_diff 'app/models/calendar.rb', :and => '15d2edc3afd7a9508a6a4b6248507234e6e893ea'
 end
 
 もし /^マイグレーション$/ do |string|
