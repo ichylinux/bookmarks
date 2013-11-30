fd76becefbf8d91f7ed9a05a1829ca7c675f847f
ichy <ichylinux@gmail.com>
2013-11-30 22:22:10 +0900

    新しいスプリント

--- features/step_definitions/開発日記/2013-11-16.rb
+++
@@ -1,8 +0,0 @@
-# coding: UTF-8
-
-もし /^小さな画面サイズではパディングをなくす$/ do
-  git_diff 'app/assets/stylesheets/welcome.css.scss',
-      :and => '541cc3e3c9f6ba384e67f148bb7d866645d165df',
-      :to => 37
-end
-  
--- features/step_definitions/開発日記/2013-11-29.rb
+++
@@ -1,9 +0,0 @@
-# coding: UTF-8
-
-もし /^今日かどうかの判定してCSSクラスを設定$/ do
-  git_diff 'app/helpers/calendars_helper.rb'
-end
-
-もし /^CSSの定義$/ do
-  git_diff 'app/assets/stylesheets/calendars.css.scss'
-end
