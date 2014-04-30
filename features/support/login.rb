bc7b5834c03efcb4bb704dabcac2530ce9e88551
y-matsuda <yuuichi.matsuda@ulsystems.co.jp>
2014-04-30 13:02:06 +0900

    テスト

---
+++ features/support/login.rb
@@ -0,0 +1,17 @@
+module Login
+
+  def sign_in(user)
+    visit '/users/sign_in'
+    fill_in 'Email', :with => user.email
+    fill_in 'Password', :with => 'testtest'
+    click_on 'Sign in'
+
+    @_current_user = user
+  end
+
+  def current_user
+    @_current_user
+  end
+end
+
+World(Login)
