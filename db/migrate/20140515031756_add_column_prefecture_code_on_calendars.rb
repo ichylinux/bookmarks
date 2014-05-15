2171dd118aff970511e53b02dd078445e3486003
y-matsuda <yuuichi.matsuda@ulsystems.co.jp>
2014-05-15 12:51:45 +0900

    都道府県の選択

---
+++ db/migrate/20140515031756_add_column_prefecture_code_on_calendars.rb
@@ -0,0 +1,9 @@
+class AddColumnPrefectureCodeOnCalendars < ActiveRecord::Migration
+  def up
+    add_column :calendars, :prefecture_code, :string, :limit => 2
+  end
+
+  def down
+    remove_column :calendars, :prefecture_code
+  end
+end
