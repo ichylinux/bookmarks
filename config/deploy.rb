f6d077aba9eff6474388a3b7dfd8ec4b702ec467
ichy <ichylinux@gmail.com>
2014-11-02 11:13:08 +0900

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.2.1'
 
 set :application, 'bookmarks'
-set :repo_url, 'git://github.com/ichylinux/bookmarks.git'
+set :repo_url, `git config --get remote.origin.url`
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
