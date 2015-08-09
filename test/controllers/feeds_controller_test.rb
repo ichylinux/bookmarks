6209a7cf1a08bdd4e275dcdb2a2d3ce05f077d8a
ichy <ichylinux@gmail.com>
2015-08-09 11:23:45 +0900

    テスト追加

--- test/controllers/feeds_controller_test.rb
+++ test/controllers/feeds_controller_test.rb
@@ -2,6 +2,13 @@ require 'test_helper'
 
 class FeedsControllerTest < ActionController::TestCase
 
+  def test_一覧
+    sign_in user
+    get :index
+    assert_response :success
+    assert_template :index
+  end
+
   def test_他人のフィードは参照できない
     sign_in user
     assert feed = Feed.where('user_id <> ?', user).first
@@ -9,6 +16,28 @@ class FeedsControllerTest < ActionController::TestCase
     get :show, :id => feed.id
 
     assert_response :not_found
+  end
+
+  def test_追加
+    sign_in user
+    get :new
+    assert_response :success
+    assert_template :new
+  end
+
+  def test_登録
+    sign_in user
+    post :create, :feed => feed_of(user).attributes
+    assert_response :redirect
+    assert @feed = assigns(:feed)
+    assert_redirected_to :action => 'index'
+  end
+
+  def test_編集
+    sign_in user
+    get :edit, :id => feed_of(user)
+    assert_response :success
+    assert_template :edit
   end
 
   def test_更新
