8c2aa70b496a19d828b2265898d51b4494310aca
ichy <ichylinux@gmail.com>
2015-08-09 11:41:09 +0900

    テスト追加

--- test/controllers/todos_controller_test.rb
+++ test/controllers/todos_controller_test.rb
@@ -2,6 +2,13 @@ require 'test_helper'
 
 class TodosControllerTest < ActionController::TestCase
 
+  def test_一覧
+    sign_in user
+    get :index
+    assert_response :success
+    assert_template :index
+  end
+
   def test_他人のタスクは編集できない
     sign_in user
     assert todo = Todo.where('user_id <> ?', user).first
