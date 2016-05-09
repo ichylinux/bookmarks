f51d6ff8cadc815af9e7387d9e566a1193648e61
ichy <ichylinux@gmail.com>
2016-05-10 01:05:37 +0900

    bundle update

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.4.0'
+lock '3.5.0'
 
 set :application, 'bookmarks'
 set :repo_url, `git config --get remote.origin.url`
@@ -14,12 +14,12 @@ set :deploy_to, "/var/apps/#{fetch(:application)}"
 # Default value for :scm is :git
 # set :scm, :git
 
-# Default value for :format is :pretty
-# set :format, :pretty
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
 
-# Default value for :log_level is :debug
-# set :log_level, :debug
-set :log_level, ENV['LOG_LEVEL'] || :info
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
 
 # Default value for :pty is false
 # set :pty, true
@@ -53,8 +53,6 @@ namespace :deploy do
       execute "sudo service unicorn_bookmarks_pro restart"
     end
   end
-
-  after :publishing, :restart
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
