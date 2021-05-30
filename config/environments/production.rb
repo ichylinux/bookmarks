020c98b7ea90f9c8d4572e0eed4bdac793be34d4
ichy <ichylinux@gmail.com>
2021-05-30 21:09:25 +0900

    smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = Rails.application.secrets.default_url_options
   config.action_mailer.smtp_settings = Rails.application.secrets.smtp_settings
+  config.action_mailer.default_options = {from: ENV['SMTP_FROM'] || 'from@example.com' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
