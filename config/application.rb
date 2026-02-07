4b0bef0c2a6c2465685192d2ff022da9dc6ef0b0
ichy <ichylinux@gmail.com>
2026-02-08 01:10:06 +0900

    bin/rails app:update

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,16 +11,23 @@ module Bookmarks
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
 
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
 
     I18n.config.enforce_available_locales = true
     config.i18n.default_locale = :ja
-
-    config.action_controller.action_on_unpermitted_parameters = :raise
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_08_042957) do
+ActiveRecord::Schema[7.2].define(version: 2026_02_07_160619) do
   create_table "bookmarks", charset: "utf8mb3", collation: "utf8mb3_unicode_ci", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "title", null: false
@@ -124,5 +124,4 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_08_042957) do
     t.index ["otp_secret_key"], name: "index_users_on_otp_secret_key", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
