8c91d832257168293c00d54d590f3920d2f6acd1
ichy <ichylinux@gmail.com>
2026-02-08 13:01:10 +0900

    リファクタリング

--- config/application.rb
+++ config/application.rb
@@ -27,9 +27,9 @@ module Bookmarks
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
 
-    config.active_record.encryption.primary_key = ENV.fetch('ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY', 'EAMuNCRks7EsAChw6tU2vLLJdsDVy1fZ')
-    config.active_record.encryption.deterministic_key = ENV.fetch('ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY', 'A0VOsmcE4csVdgw3mL1YCedP1oroHQCG')
-    config.active_record.encryption.key_derivation_salt = ENV.fetch('ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT', 'YcWgF14pbnXyWajYUrxZvIefDwm67Ey6')
+    config.active_record.encryption.primary_key = ENV.fetch('ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY', 'dev_dummy_key')
+    config.active_record.encryption.deterministic_key = ENV.fetch('ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY', 'dev_dummy_key')
+    config.active_record.encryption.key_derivation_salt = ENV.fetch('ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT', 'dev_dummy_key')
 
     I18n.config.enforce_available_locales = true
     config.i18n.default_locale = :ja
