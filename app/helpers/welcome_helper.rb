603a16ecda06cfd94f339d3a7f00f8560e51450f
ichy <ichylinux@gmail.com>
2026-04-30 01:32:34 +0900

    refactor

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,8 +1,8 @@
 module WelcomeHelper
 
   def favorite_theme
-    return nil unless user_signed_in?
-    return nil unless current_user.preference.present?
-    return current_user.preference.theme
+    return 'modern' unless user_signed_in?
+    return 'modern' unless current_user.preference.present?
+    return current_user.preference.theme.presence || 'modern'
   end
 end
