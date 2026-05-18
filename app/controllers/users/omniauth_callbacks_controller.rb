38eee50924e9038ac08f2b0f05e9315bfd1c050e
ichylinux <ichylinux@gmail.com>
2026-05-18 18:28:24 +0900

    ログイン成功メッセージは不要

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -14,7 +14,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: kind
       sign_in_and_redirect @user, event: :authentication
     else
       session["devise.#{kind.downcase}_data"] = request.env["omniauth.auth"].except("extra")
