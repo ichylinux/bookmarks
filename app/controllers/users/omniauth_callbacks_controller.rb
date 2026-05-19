dd8e3dbb39fd00f2671eee38e673e8520d991d1e
ichylinux <ichylinux@gmail.com>
2026-05-19 19:22:36 +0900

    fix(02): CR-01 fix 'Upgrade' flow and account clashing in twitter2 callback

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -10,10 +10,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def twitter2
     auth = request.env["omniauth.auth"]
-    @user = current_user || User.from_omniauth(auth)
-
-    unless @user.persisted?
-      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+    uid = auth.uid.to_s
+    
+    # Check if this X account is already linked to another user
+    existing_user = User.where(uid: uid, provider: %w[twitter twitter2]).where.not(id: current_user&.id).first
+    if existing_user
+      redirect_to x_accounts_path, alert: "This X account is already linked to another user."
       return
     end
 
@@ -21,7 +23,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       # Upgrade: store OAuth 2.0 tokens on the already-signed-in user
       creds = auth.credentials || {}
       expires_at = creds['expires_at'] ? Time.at(creds['expires_at'].to_i) : nil
+      
+      # Correctly update provider and uid if they were missing or different
       current_user.assign_attributes(
+        provider: 'twitter2',
+        uid: uid,
         oauth2_token: creds['token'],
         oauth2_refresh_token: creds['refresh_token'],
         oauth2_token_expires_at: expires_at
@@ -29,6 +35,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       current_user.save(validate: false)
       redirect_to x_accounts_path, notice: t('x_accounts.oauth2_upgraded')
     else
+      @user = User.from_omniauth(auth)
       sign_in_and_redirect @user, event: :authentication
     end
   end
