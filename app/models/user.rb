a707a74a05ca37607e22ef618185dcfb602caca2
ichy <ichylinux@gmail.com>
2026-05-24 23:11:26 +0900

    v1.34.0

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,7 @@ class User < ApplicationRecord
       .where('deleted_at <= ?', PURGE_AFTER_DAYS.days.ago)
   }
 
-  has_many :oauth_identities, dependent: :destroy
+  has_many :oauth_identities
 
   has_one :preference, inverse_of: 'user'
   accepts_nested_attributes_for :preference
