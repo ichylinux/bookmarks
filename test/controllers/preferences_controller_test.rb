c53092d7d5d96d891781c7f0bd7d081bf050bad6
ichylinux <ichylinux@gmail.com>
2026-05-26 16:32:57 +0900

    デザイン調整

--- test/controllers/preferences_controller_test.rb
+++ test/controllers/preferences_controller_test.rb
@@ -373,7 +373,7 @@ class PreferencesControllerTest < ActionDispatch::IntegrationTest
                   text: I18n.t('preferences.index.connected_accounts_section_title', locale: :ja)
     assert_select 'section.connected-accounts .connected-accounts__row', count: 4
     %w[google twitter facebook email_password].each do |key|
-      assert_select 'section.connected-accounts th',
+      assert_select 'section.connected-accounts .connected-accounts__provider',
                     text: I18n.t("preferences.index.connected_accounts.#{key}", locale: :ja)
     end
   end
