3bbabf1a307426ecb816682e92af59748a710eb7
ichy <ichylinux@gmail.com>
2026-06-17 01:42:32 +0900

    リファクタリング

--- db/migrate/20260617000001_remove_uid_and_provider_from_users.rb
+++ db/migrate/20260617000001_remove_uid_and_provider_from_users.rb
@@ -1,19 +1,5 @@
 class RemoveUidAndProviderFromUsers < ActiveRecord::Migration[8.1]
-  class MigrationUser < ApplicationRecord
-    self.table_name = 'users'
-  end
-
-  class MigrationOauthIdentity < ApplicationRecord
-    self.table_name = 'oauth_identities'
-  end
-
   def up
-    MigrationUser.where.not(provider: nil).where.not(uid: nil).find_each do |user|
-      MigrationOauthIdentity.find_or_create_by!(user_id: user.id, provider: user.provider) do |identity|
-        identity.uid = user.uid
-      end
-    end
-
     remove_index :users, :uid, if_exists: true
     remove_column :users, :uid
     remove_column :users, :provider
@@ -23,12 +9,5 @@ class RemoveUidAndProviderFromUsers < ActiveRecord::Migration[8.1]
     add_column :users, :provider, :string
     add_column :users, :uid, :string
     add_index :users, :uid, unique: true
-
-    MigrationOauthIdentity.find_each do |identity|
-      MigrationUser.where(id: identity.user_id).update_all(
-        provider: identity.provider,
-        uid: identity.uid
-      )
-    end
   end
 end
